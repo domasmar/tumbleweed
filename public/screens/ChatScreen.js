4f1d6c368e93f4967db31ff2aac8906c2e9868c0
edgarasbuchvalovas <edgaras.buchvalovas@visma.com>
2018-10-21 01:09:36 +0300

    not working chat

--- public/screens/ChatScreen.js
+++ public/screens/ChatScreen.js
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import {GiftedChat} from 'react-native-gifted-chat'
 
-
 class ChatScreen extends React.Component {
     static navigationOptions = {
         title: 'Chat',
@@ -11,21 +10,45 @@ class ChatScreen extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            open: false
+        };
+
+        this.socket = new WebSocket('ws://tumbleweed-hack.herokuapp.com/chat', '', {
+            headers: {
+                "chat-sender": "AAA",
+                "chat-receiver": "BBB"
+            }
+        });
+
+        this.emit = this.emit.bind(this);
+    }
+
+    emit() {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }));
+        this.socket.send("It worked!")
     }
 
     state = {
         messages: [],
+        open: false,
+        connected: false
     };
 
     componentWillMount() {
+        this.socket.onopen = () => console.log("Connected to WS");//this.socket.send(JSON.stringify({type: 'greet', payload: 'Hello Mr. Server!'}));
+        this.socket.onmessage = ({data}) => console.log(data);
         this.setState({
             messages: [
                 {
-                    _id: 1,
+                    _id: "111",
                     text: 'Hello developer',
                     createdAt: new Date(),
                     user: {
-                        _id: 2,
+                        _id: "111",
                         name: 'React Native'
                     },
                 },
@@ -33,7 +56,20 @@ class ChatScreen extends React.Component {
         })
     }
 
+    toClientMessages(data) {
+
+    }
+
+    toServerMessage(message) {
+        let msgText = message[0].text;
+        return {
+            "text": msgText
+        }
+
+    }
+
     onSend(messages = []) {
+        this.socket.send(JSON.stringify(this.toServerMessage(messages)));
         this.setState(previousState => ({
             messages: GiftedChat.append(previousState.messages, messages),
         }))
@@ -45,7 +81,7 @@ class ChatScreen extends React.Component {
                 messages={this.state.messages}
                 onSend={messages => this.onSend(messages)}
                 user={{
-                    _id: 1,
+                    _id: "AAA",
                 }}
             />
         )
