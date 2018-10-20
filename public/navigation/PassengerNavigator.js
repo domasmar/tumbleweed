8b4cb2aa9c89caed180aa9c4c70dfce977a08314
Domas Martisius <domas933@gmail.com>
2018-10-20 14:55:35 +0300

    landing page

---
+++ public/navigation/PassengerNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import MapScreen from '../screens/MapScreen';
+
+const MapStack = createStackNavigator({
+    Map: MapScreen,
+});
+
+MapStack.navigationOptions = {
+    tabBarLabel: 'Search route',
+    tabBarIcon: ({ focused }) => (
+        <TabBarIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? `ios-car${focused ? '' : '-outline'}` : 'md-car'}
+        />
+    ),
+};
+
+export default createBottomTabNavigator({
+    MapStack,
+});
