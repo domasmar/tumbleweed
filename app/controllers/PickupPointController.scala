40ebc746d942b3e4012354faf5d2c3674874edcb
Domas Martisius <domas933@gmail.com>
2018-10-20 10:25:16 +0300

    sbt native plugin. pretty output

--- app/controllers/PickupPointController.scala
+++ app/controllers/PickupPointController.scala
@@ -15,12 +15,12 @@ class PickupPointController @Inject()(
 
   def getAll() = Action.async { implicit request: Request[AnyContent] =>
     val all = pickupPointService.getAll()
-    all.map(a => Ok(mapper.writeValueAsString(a)))
+    all.map(a => Ok(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(a)))
   }
 
   def get(lat: Double, long: Double, maxDistance: Double) = Action.async { implicit request: Request[AnyContent] =>
     val all = pickupPointService.getAll(Point(lat, long), maxDistance)
-    all.map(a => Ok(mapper.writeValueAsString(a)))
+    all.map(a => Ok(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(a)))
   }
 
 }
